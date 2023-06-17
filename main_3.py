2caed8f879191c397c08ccc28ef92a6042207b29
ilyasqn <123233396+ilyasqn@users.noreply.github.com>
2023-06-17 13:14:49 +0600

    OOP_3

---
+++ main_3.py
@@ -0,0 +1,9 @@
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def display(self):
+        print(f"Name: {self.name}, Age: {self.age}")
+
+student = Student("Alice", 20)
+student.display()
